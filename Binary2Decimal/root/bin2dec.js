e071336581f79f9cec4d6190e49f15dc9abf7be6
mercedesscilingo <mercedes.scilingo@gmail.com>
2022-07-15 16:13:58 -0300

    issues solved

--- Binary2Decimal/root/bin2dec.js
+++ Binary2Decimal/root/bin2dec.js
@@ -1,20 +1,20 @@
 const prompt = require("prompt-sync")({ sigint: true });
 
-function convertBinaryToDecimal (string){
-    string = prompt("Enter a binary number up to 8 digits: ");
-    let binary = string.split('').filter(function(elemento){return elemento === "1" || elemento === "0"});
+const binaryNumber = prompt("Enter a binary number up to 8 digits: ");
 
-    if (string.length !== binary.length || string.length > 8){
+function convertBinaryToDecimal (number){
+    const onlyBinary = (element) => element === "1" || element === "0";
+    const binary = number.split('').filter(onlyBinary(number));
+    if (number.length !== binary.length || number.length > 8){
         return "Error, please enter a valid number.";
     }
     else {
-        let decimal = parseInt(string, 2);
+        let decimal = parseInt(number, 2);
         return decimal
     }
 };
 
-
-let decimalResults = convertBinaryToDecimal();
+let decimalResults = convertBinaryToDecimal(binaryNumber);
 console.log(decimalResults);
 
 
