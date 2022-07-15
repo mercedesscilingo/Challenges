c7d5fa16df0b2f3ed2330bd3da29618a26ba1e37
mercedesscilingo <mercedes.scilingo@gmail.com>
2022-07-15 17:21:38 -0300

    Corrected code

--- Binary2Decimal/bin2dec.js
+++ Binary2Decimal/bin2dec.js
@@ -3,8 +3,7 @@ const prompt = require("prompt-sync")({ sigint: true });
 const binaryNumber = prompt("Enter a binary number up to 8 digits: ");
 
 function convertBinaryToDecimal (number){
-    const onlyBinary = (element) => element === "1" || element === "0";
-    const binary = number.split('').filter(onlyBinary(number));
+    const binary = number.split('').filter((element) => element === "1" || element === "0");
     if (number.length !== binary.length || number.length > 8){
         return "Error, please enter a valid number.";
     }
@@ -16,4 +15,3 @@ function convertBinaryToDecimal (number){
 
 let decimalResults = convertBinaryToDecimal(binaryNumber);
 console.log(decimalResults);
-
