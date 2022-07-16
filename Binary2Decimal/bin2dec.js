c0aa9725173d8554bf20e7102ead634c22bb0468
mercedesscilingo <mercedes.scilingo@gmail.com>
2022-07-16 00:21:55 -0300

    added files

--- Binary2Decimal/bin2dec.js
+++ Binary2Decimal/bin2dec.js
@@ -3,15 +3,16 @@ const prompt = require("prompt-sync")({ sigint: true });
 const binaryNumber = prompt("Enter a binary number up to 8 digits: ");
 
 function convertBinaryToDecimal (number){
-    const binary = number.split('').filter((element) => element === "1" || element === "0");
+    const onlyBinary = (element) => (element === "1" || element === "0");
+    const binary = number.split('').filter(onlyBinary);
     if (number.length !== binary.length || number.length > 8){
         return "Error, please enter a valid number.";
     }
     else {
-        let decimal = parseInt(number, 2);
+        const decimal = parseInt(number, 2);
         return decimal
     }
 };
 
-let decimalResults = convertBinaryToDecimal(binaryNumber);
+const decimalResults = convertBinaryToDecimal(binaryNumber);
 console.log(decimalResults);
